7ec5dde8402653d6cb038116074d543023f000b5
Srujal <sujalshte13@gmail.com>
2024-02-01 17:22:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# iNotes---The-Notes-App
---
+++ about.php
@@ -0,0 +1,58 @@
+<?php
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iNotes</title>
+    <link rel="stylesheet" href="img/styles.css">
+    <link rel="icon" type="image/png" href="img/INotes.png">
+</head>
+<body>
+
+<header>
+    <h1>About Our iNotes App</h1>
+</header>
+
+<section>
+    <h2><center>Overview</h2>
+    <p><center>
+        Welcome to our Notes Application! We understand the importance of keeping your thoughts organized, and that's why we've created this simple and intuitive notes platform.
+    </p>
+</section>
+
+<section>
+
+    <h2><center>Features</h2>
+    <ul class="centered-list">
+        <li>Easy-to-use interface for creating and managing notes.</li>
+        <li>Secure account system to keep your notes private.</li>
+        <li>Customizable categories and tags for efficient organization.</li>
+        <li>Quick search functionality to find your notes in seconds.</li>
+        <li>Responsive design for seamless use on various devices.</li>
+    </ul>
+</section>
+ 
+    <section>
+        <h2><center>Our Mission</center></h2>
+        <p><center>
+            Our mission is to provide a reliable and user-friendly platform for individuals to capture and organize their thoughts, ideas, and important</br> information effortlessly. We believe that a well-designed notes application can enhance productivity and bring clarity to your daily life.
+        </p>
+    </section>
+
+<footer class="centered-footer">
+    <div class="footer-background">
+        <p>&copy; <?php echo date("Y"); ?> INotes App Developed By Srujal</p>
+    </div>
+</footer>
+
+
+</body>
+</html>
+
+
+	<!-- End Blog -->
+<?php
+?>
